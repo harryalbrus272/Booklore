41bfcc344a032dcb0ee7b4d8c5f22f715857ed5c
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-27 10:12:12 +0530

    add initial components to be used

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar';
+export { default as Home } from './Home';
+export { default as Signin } from './Signin';
+export { default as Signup } from './Signup';
+export { default as Order } from './Order';
+export { default as Page404 } from './Page404';
