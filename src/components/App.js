1b3c3370225ae7eb4693e9d4b48c44ff4ae116ee
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-28 14:37:51 +0530

    remove unnecessary code from the app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -7,33 +7,6 @@ import {
 } from 'react-router-dom';
 import { Navbar, Home, Order, Signin, Signup, Page404 } from './';
 function App() {
-  let password = 'something';
-  let email = 'shashwatksingh.27@gmail.com';
-  function getFormBody(params) {
-    console.log(params);
-    let formBody = [];
-    for (let property in params) {
-      let encodedKey = encodeURIComponent(property);
-      let encodedValue = encodeURIComponent(params[property]);
-
-      formBody.push(encodedKey + '=' + encodedValue);
-    }
-    return formBody.join('&');
-  }
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: getFormBody({
-        password,
-        email,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
   return (
     <Router>
       <div className="App">
